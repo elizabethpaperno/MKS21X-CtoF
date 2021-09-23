a9b3506b0789a4582087be6f06c4aceb350c75e6
Elizabeth Paperno <liza.paperno@gmail.com>
2021-09-23 16:58:51 -0400

    Created class and added methods

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,10 @@
 // The celsiusToFahrenheit() should take degrees in Celsius as a parameter and return degrees in fahrenheit. The method fahrenheitToCelsius() should take degrees in fahrenheit as a parameter, and return degrees in Celsius. These parameters should be passed in as doubles, because degrees can be floating point numbers. The return type should also be a double, sd this is required to maintain accuracy.
+
+public class CtoFTester{
+  public static double celsiusToFahrenheit(double cel){
+
+  }
+  public static double fahrenheitToCelsius(double cel){
+
+  }
+}
